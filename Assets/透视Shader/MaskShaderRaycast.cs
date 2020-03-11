7d346313f428b0d1082bba9dc3c5e48cf0f59021
C.y <yangruihan@vip.qq.com>
2020-03-11 08:24:15 +0800

    添加透视Demo

---
+++ Assets/透视Shader/MaskShaderRaycast.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskShaderRaycast : MonoBehaviour
+{
+    public Transform tf;
+    public Transform tf2;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit raycastHit;
+        var dis = (tf.position - _camera.transform.position).magnitude;
+        if (Physics.Raycast(new Ray(_camera.transform.position, _camera.transform.forward), out raycastHit, dis))
+        {
+            if (raycastHit.transform == tf)
+            {
+                tf2.localScale = Vector3.zero;
+            }
+            else
+            {
+                tf2.localScale = new Vector3(2, 2, 2);
+            }
+        }
+    }
+}
